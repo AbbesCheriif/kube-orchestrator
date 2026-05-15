ab29d32eb16cb53c95e9f2cf22b27831554947e5
abbescheriif <abbes.cherif@enis.tn>
2026-05-15 22:28:37 +0200

    feat: implement ResourceQuotaManager with all fields

--- kube_orchestrator/resources/cluster/__init__.py
+++ kube_orchestrator/resources/cluster/__init__.py
@@ -1,3 +1,4 @@
 from kube_orchestrator.resources.cluster.namespace import NamespaceManager
+from kube_orchestrator.resources.cluster.resource_quota import ResourceQuotaManager
 
-__all__ = ["NamespaceManager"]
+__all__ = ["NamespaceManager", "ResourceQuotaManager"]
